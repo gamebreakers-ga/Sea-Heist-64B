0b81845733cae893701d09ff4ce0360d2b032787
sammwie21 <sammwie21@gmail.com>
2026-05-02 14:51:48 -0400

    basic fish

---
+++ Assets/fihscript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fihscript : MonoBehaviour
+{
+    public int cost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cost = Random.Range(1500, 2001);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
